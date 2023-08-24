f7bd288febeebe65bb336c61a089ecce2b04f738
guilhermebodin <guilherme.b.moraes@gmail.com>
2023-08-24 12:43:11 -0300

    Add formatter as a critical job

--- formatter/format.jl
+++ formatter/format.jl
@@ -3,7 +3,23 @@ Pkg.instantiate()
 
 using JuliaFormatter
 
-formatted_src = format(joinpath(dirname(@__DIR__), "src"))
-formatted_test = format(joinpath(dirname(@__DIR__), "test"))
-println("src folder formatted: $formatted_src")
-println("test folder formatted: $formatted_test")
+import Pkg
+Pkg.instantiate()
+
+using JuliaFormatter
+
+function apply_formatter()
+    # 3 times to converge formatting is an heuristic
+    println("Formatting src and test folders")
+    for _ in 1:2
+        format(joinpath(dirname(@__DIR__), "src"))
+        format(joinpath(dirname(@__DIR__), "test"))
+    end
+    formatted_src = format(joinpath(dirname(@__DIR__), "src"))
+    formatted_test = format(joinpath(dirname(@__DIR__), "test"))
+    println("src folder formatted: $formatted_src")
+    println("test folder formatted: $formatted_test")
+    return nothing
+end
+
+apply_formatter()
